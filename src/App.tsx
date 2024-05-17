28a52f8a075d12db5e636a1a4cd0a082d329233b
Splicid <luis1abreu11@gmail.com>
2024-05-16 21:16:19 -0400

    working user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,63 @@
-import { useState } from 'react';
+import { useState, ChangeEvent  } from 'react';
+import React from 'react'
 import './App.css'
 
-async function App() {
-  const apiKey = import.meta.env.VITE_REACT_API_URL;
-  console.log(apiKey)
-  const url = "http://api.weatherapi.com/v1"
-  let result = await fetch(`${url}/current.json?key=${apiKey}&q=London&aqi=no`)
-  let data = await result.json()
-  console.log(data)
-  return (
-    <div> Test </div>
-  )
+const apiKey = import.meta.env.VITE_REACT_API_URL;
+
+interface myStates {
+  weather: any,
+  location: string,
+  zipcode: number,
+}
+
+class App extends React.Component<{}, myStates> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      weather: null,
+      location: "",
+      zipcode: 0,
+    }
+  }
+
+  handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    const url = "http://api.weatherapi.com/v1"
+    const location = this.state.location
+    let result = await fetch(`${url}/current.json?key=${apiKey}&q=${location}&aqi=no`)
+    let data = await result.json()
+    this.setState({ weather: data })
+  }
+
+  render() {
+    console.log(this.state.location)
+    return (
+      <div>
+        <h1>Weather App</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+          type="text"
+          placeholder="Enter a location"
+          value={this.state.location}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => this.setState({location: e.target.value})}
+           />
+          <button>Search</button>
+        </form>
+
+        <h2>Current Weather</h2>
+        {this.state.weather ? (
+          <div>
+            <h3>{this.state.weather.location.name}</h3>
+            <p>{this.state.weather.current.temp_c}°C</p>
+            <img src={this.state.weather.current.condition.icon} alt="weather icon" />
+            <p>{this.state.weather.current.condition.text}</p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
